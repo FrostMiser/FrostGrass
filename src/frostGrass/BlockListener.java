cc8ed646303ab9393ab1f778c3ee80ee3b53ddca
FrostMiser <FrostMiser@users.noreply.github.com>
2020-04-26 12:17:00 -0500

    Fix spacing

--- src/frostGrass/BlockListener.java
+++ src/frostGrass/BlockListener.java
@@ -7,16 +7,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockSpreadEvent;
 
 public class BlockListener implements Listener {
-	
 	@EventHandler
 	public void onGrassSpread(BlockSpreadEvent event) {
 		if (event.getSource().getType().equals(Material.GRASS)) {
 			Location locAbove = event.getBlock().getLocation().clone().add(0,1,0);
 			if (locAbove.getBlock().getType().equals(Material.AIR)) {
 				locAbove.getBlock().setType(Material.SNOW);
-				
 			}
 		}
 	}
-		
+
 }
